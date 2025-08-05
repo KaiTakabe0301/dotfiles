244ee4b681e2fb803aadfc6b9abd875c66078c48
KaiTakabe0301 <tb12e053.tkb@gmail.com>
2025-08-05 21:31:26 +0900

    add: Neovim(nvchad)のカスタムプラグイン設定ファイルを追加

---
+++ home/dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,59 @@
+-- Custom plugins configuration
+
+return {
+  -- Mason-lspconfig for automatic LSP installation
+  {
+    "mason-org/mason-lspconfig.nvim",
+     config = function()
+       require "configs.lspconfig"
+     end,
+    lazy = false,
+    opts = {
+      automatic_installation = true,
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        { "neovim/nvim-lspconfig", opts = {} },
+    },
+  },
+
+  -- nvim-vtsls for TypeScript
+  {
+    "yioneko/nvim-vtsls",
+    lazy = true,
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+      require("vtsls").config({
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  -- Enhanced TreeSitter language support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "javascript", "typescript", "tsx", "json",
+        "python", "go", "rust", "yaml", "toml", "markdown", "bash"
+      },
+    },
+  },
+}
---
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- Load all custom plugins
+  require("custom.plugins"),
+}
