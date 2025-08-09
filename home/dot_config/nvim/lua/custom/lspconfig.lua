7c88e9cdb9b7711609550a607bbf190102f55e2a
KaiTakabe0301 <tb12e053.tkb@gmail.com>
2025-08-10 01:54:37 +0900

    refactor: LSP設定を整理し、vtslsの設定を強化

--- home/dot_config/nvim/lua/custom/lspconfig.lua
+++ home/dot_config/nvim/lua/custom/lspconfig.lua
@@ -6,22 +6,6 @@ M.setup = function()
   local lspconfig = require "lspconfig"
   local nvlsp = require "nvchad.configs.lspconfig"
 
-  -- Configure mason-lspconfig
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "lua_ls",
-      "vtsls",
-      "html",
-      "cssls",
-      "pyright",
-      "gopls",
-      "rust_analyzer",
-      "yamlls",
-      "jsonls",
-      "bashls",
-    },
-  })
-
   -- LSP servers with default config
   local servers = {
     "html",
@@ -42,39 +26,6 @@ M.setup = function()
       capabilities = nvlsp.capabilities,
     }
   end
-
-  -- TypeScript/JavaScript with vtsls
-  lspconfig.vtsls.setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-    settings = {
-      complete_function_calls = true,
-      vtsls = {
-        enableMoveToFileCodeAction = true,
-        autoUseWorkspaceTsdk = true,
-        experimental = {
-          completion = {
-            enableServerSideFuzzyMatch = true,
-          },
-        },
-      },
-      typescript = {
-        updateImportsOnFileMove = { enabled = "always" },
-        suggest = {
-          completeFunctionCalls = true,
-        },
-        inlayHints = {
-          enumMemberValues = { enabled = true },
-          functionLikeReturnTypes = { enabled = true },
-          parameterNames = { enabled = "literals" },
-          parameterTypes = { enabled = true },
-          propertyDeclarationTypes = { enabled = true },
-          variableTypes = { enabled = false },
-        },
-      },
-    },
-  }
 
   -- Lua with enhanced settings
   lspconfig.lua_ls.setup {
--- home/dot_config/nvim/lua/custom/plugins.lua
+++ home/dot_config/nvim/lua/custom/plugins.lua
@@ -18,6 +18,18 @@ return {
     lazy = false,
     opts = {
       automatic_installation = true,
+      ensure_installed = {
+        "lua_ls",
+        "vtsls",
+        "html",
+        "cssls",
+        "pyright",
+        "gopls",
+        "rust_analyzer",
+        "yamlls",
+        "jsonls",
+        "bashls",
+      },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
@@ -28,30 +40,70 @@ return {
   -- nvim-vtsls for TypeScript
   {
     "yioneko/nvim-vtsls",
-    lazy = true,
+    lazy = false,
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
-      require("vtsls").config({
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
+      local nvlsp = require("nvchad.configs.lspconfig")
+
+      -- 既定サーバ定義の登録（公式推奨）
+      require("lspconfig.configs").vtsls = require("vtsls").lspconfig
+
+      -- 参照にも色を効かせたい人向け（任意）
+      local function on_attach_with_semantic(client, bufnr)
+        if nvlsp.on_attach then nvlsp.on_attach(client, bufnr) end
+        if client.supports_method("textDocument/semanticTokens/full") then
+          vim.lsp.semantic_tokens.start(bufnr, client.id)
+          vim.defer_fn(function() pcall(vim.lsp.semantic_tokens.refresh, bufnr) end, 50)
+        end
+      end
+
+      lspconfig.vtsls.setup {
+        on_attach = on_attach_with_semantic,  -- 既存の on_attach をラップ
+        on_init = nvlsp.on_init,
+        capabilities = nvlsp.capabilities,
+
+        -- ★ これが肝：最寄り tsconfig を“優先”してパッケージ単位で root を切る
+        root_dir = function(fname)
+          return util.root_pattern("tsconfig.json")(fname)
+              or util.root_pattern("package.json", "jsconfig.json")(fname)
+              or util.find_git_ancestor(fname)
+        end,
+        single_file_support = false,
+
         settings = {
           complete_function_calls = true,
           vtsls = {
             enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true, -- ★ 各パッケージの node_modules/typescript を自動使用
             experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
+              completion = { enableServerSideFuzzyMatch = true },
             },
           },
           typescript = {
             updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
+            suggest = { completeFunctionCalls = true },
+            -- pnpm/yarn Workspace でローカル plugin を見つけやすくする保険（任意）
+            tsserver = { pluginPaths = { "." } },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
             },
           },
         },
-      })
+      }
     end,
-  },
+  }
+
+
+
+
+  ,
 
   -- Enhanced TreeSitter language support
   {
