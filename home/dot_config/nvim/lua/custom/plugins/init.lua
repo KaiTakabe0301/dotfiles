d9ae5a418f77ac34a1290db9d3f46837e1b67197
KaiTakabe0301 <tb12e053.tkb@gmail.com>
2025-08-11 02:19:00 +0900

    feat(nvim): nvim-treeの設定を削除

--- home/dot_config/nvim/lua/custom/plugins/init.lua
+++ home/dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,30 +2,28 @@
 
 -- Load custom init script
 vim.schedule(function()
-  local init_path = vim.fn.stdpath("config") .. "/lua/custom/init.lua"
+  local init_path = vim.fn.stdpath "config" .. "/lua/custom/init.lua"
   if vim.fn.filereadable(init_path) == 1 then
     dofile(init_path)
   end
 end)
 
 return {
-  require("custom.plugins.mason-lspconfig"),
+  require "custom.plugins.mason-lspconfig",
 
-  require("custom.plugins.nvim-vtsls"),
+  require "custom.plugins.nvim-vtsls",
 
-  require("custom.plugins.nvim-treesitter"),
+  require "custom.plugins.nvim-treesitter",
 
-  require("custom.plugins.none-ls"),
+  require "custom.plugins.none-ls",
 
-  require("custom.plugins.lazygit-nvim"),
+  require "custom.plugins.lazygit-nvim",
 
-  require("custom.plugins.indent-blankline"),
+  require "custom.plugins.indent-blankline",
 
-  require("custom.plugins.hlchunk"),
+  require "custom.plugins.hlchunk",
 
-  require("custom.plugins.rainbow-delimiters"),
+  require "custom.plugins.rainbow-delimiters",
 
-  require("custom.plugins.whichkey"),
-
-  require "custom.plugins.nvim-tree",
+  require "custom.plugins.whichkey",
 }
